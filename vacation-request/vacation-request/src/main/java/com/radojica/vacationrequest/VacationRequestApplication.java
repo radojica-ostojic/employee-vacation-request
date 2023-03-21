ffe43092139895707da48ef05ee6606ab7562291
radojica-ostojic <ostojicradojica21@gmail.com>
2023-03-21 12:24:42 +0100

    initial sourcetree connection

---
+++ vacation-request/vacation-request/src/main/java/com/radojica/vacationrequest/VacationRequestApplication.java
@@ -0,0 +1,13 @@
+package com.radojica.vacationrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationRequestApplication.class, args);
+	}
+
+}
---
+++ vacation-request/vacation-request/src/test/java/com/radojica/vacationrequest/VacationRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radojica.vacationrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
